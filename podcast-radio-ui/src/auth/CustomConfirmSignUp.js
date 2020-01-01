a234eefb9f723f26ae58d0ace96350556f9fecfc
drspacemanphd <markabrown87@gmail.com>
2020-01-01 13:23:42 -0500

    Added custom message, loader for messages

--- podcast-radio-ui/src/auth/CustomConfirmSignUp.js
+++ podcast-radio-ui/src/auth/CustomConfirmSignUp.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet, KeyboardAvoidingView, Dimensions } from 'react-native';
+import { View, Text, StyleSheet, KeyboardAvoidingView, Dimensions, ActivityIndicator } from 'react-native';
 import { Button, Input } from 'react-native-elements';
 import { ConfirmSignUp } from 'aws-amplify-react-native';
 import { Auth } from 'aws-amplify';
@@ -7,6 +7,56 @@ import { Auth } from 'aws-amplify';
 export default class CustomConfirmSignUp extends ConfirmSignUp {
     constructor(props) {
         super(props);
+        this.customConfirm = this.customConfirm.bind(this);
+        this.initialState = {
+            ...this.state,
+            awaitingAuthResponse: false
+        }
+        this.state = { ...this.initialState }
+    }
+
+    async customConfirm() {
+        await this.setState({
+            awaitingAuthResponse: true
+        });
+
+        try {
+            const { username, code } = this.state;
+            let data = await Auth.confirmSignUp(username, code);
+            await this.changeState('signedUp');
+            await this.setState({
+                ...this.initialState
+            });
+        } catch(err) {
+            await this.error(err);
+            await this.setState({
+                awaitingAuthResponse: false
+            });
+        }
+    }
+
+    resolveUserMessageError(errorMessage) {
+        const messagesToOverwrite = [
+            'Custom auth lambda trigger is not configured for the user pool.'
+        ];
+        if (messagesToOverwrite.includes(errorMessage)) return 'Something went wrong. Check if valid email/password are provided!';
+        else return errorMessage;
+    }
+
+    renderAuthSubmitResponse() {
+        if (!this.state.awaitingAuthResponse) {
+            return (
+                <View style={{ marginTop: 10, height: 15 }}>
+                    <Text style={{ color: 'rgba(255,155,155,1)', margin: 0, padding: 0, textAlign: 'center', fontSize: 12 }}>{!this.state.error ? '' : this.resolveUserMessageError(this.state.error)}</Text>
+                </View>
+            );
+        } else {
+            return (
+                <View style={{ marginTop: 10, height: 15 }}>
+                    <ActivityIndicator color='white' style={{ margin: 0, padding: 0 }} />
+                </View>
+            );
+        }
     }
 
     showComponent(theme) {
@@ -28,10 +78,11 @@ export default class CustomConfirmSignUp extends ConfirmSignUp {
                     onChangeText={(text) => this.setState({ code: text })}
                 />
                 <View style={styles.buttonContainer}>
-                    <Button type='outline' title='Confirm' buttonStyle={styles.button} titleStyle={styles.buttonTitle} onPress={this.confirm} disabled={!this.state.username || !this.state.code} />
-                    <Text style={{ color: 'white', marginTop: 25, fontSize: 15 }}>Check your email for a confirmation code. You will be asked to sign in again after confirmation.</Text>
+                    <Button type='outline' title='Confirm' buttonStyle={styles.button} titleStyle={styles.buttonTitle} onPress={this.customConfirm} disabled={!this.state.username || !this.state.code} />
+                    <Text style={{ color: 'white', marginTop: 25, fontSize: 15, textAlign: 'center' }}>Check your email for a confirmation code.</Text>
+                    <Text style={{ textDecorationLine: 'underline', color: 'white', marginTop: 15, fontSize: 15, textAlign: 'center' }}>You will be asked to sign in again after confirmation.</Text>
                 </View>
-                <Text style={{ color: 'white' }}>{this.state.error === undefined ? '' : this.state.error}</Text>
+                {this.renderAuthSubmitResponse()}
             </KeyboardAvoidingView>
         );
     }
@@ -56,11 +107,8 @@ const styles = StyleSheet.create({
         justifyContent: 'space-evenly',
     },
     buttonContainer: {
-        flex: 1,
-        maxHeight: 100,
+        maxHeight: 200,
         width: (Dimensions.get('window').width * 0.85),
-        flexDirection: 'column',
-        justifyContent: 'space-evenly',
         alignItems: 'center',
         marginTop: 20
     },
--- podcast-radio-ui/src/auth/CustomSignIn.js
+++ podcast-radio-ui/src/auth/CustomSignIn.js
@@ -1,11 +1,78 @@
 import React from 'react';
-import { StyleSheet, KeyboardAvoidingView, Dimensions, Text, View } from 'react-native';
+import { StyleSheet, KeyboardAvoidingView, Dimensions, Text, View, ActivityIndicator } from 'react-native';
 import { Input, Button } from 'react-native-elements';
 import { SignIn } from 'aws-amplify-react-native';
+import { Auth } from 'aws-amplify';
 
 export default class CustomSignIn extends SignIn {
     constructor(props) {
         super(props);
+        this.resolveUserMessageError = this.resolveUserMessageError.bind(this);
+        this.renderAuthSubmitResponse = this.renderAuthSubmitResponse.bind(this);
+        this.customSignIn = this.customSignIn.bind(this);
+        this.initialState = {
+            error: null,
+            password: null,
+            username: null,
+            awaitingAuthResponse: false
+        }
+        this.state = { ...this.initialState }
+    }
+
+    async customSignIn() {
+
+        await this.setState({
+            awaitingAuthResponse: true
+        });
+
+        const username = this.getUsernameFromInput() || '';
+        const { password } = this.state;
+
+        try {
+            const user = await Auth.signIn(username, password);
+            await this.checkContact(user);
+            await this.setState({
+                ...this.initialState
+            });
+        } catch (err) {
+            await this.error(err);
+
+            if (this.state.error === 'User is not confirmed.') {
+                const username = this.state.username
+                await this.setState({
+                    ...this.initialState
+                }, this.changeState('confirmSignUp', username));
+            }
+
+            await this.setState({
+                awaitingAuthResponse: false
+            });
+        }
+
+    }
+
+    renderAuthSubmitResponse() {
+        if (!this.state.awaitingAuthResponse) {
+            return (
+                <View style={{marginTop: 10, height: 15}}>
+                    <Text style={{ color: 'rgba(255,155,155,1)', margin: 0, padding: 0, textAlign: 'center', fontSize: 12 }}>{!this.state.error ? '' : this.resolveUserMessageError(this.state.error)}</Text>
+                </View>
+            );
+        } else {
+            return (
+                <View style={{ marginTop: 10, height: 15 }}>
+                    <ActivityIndicator color='white' style={{margin: 0, padding: 0 }} />
+                </View>
+            );
+        }
+    }
+
+    resolveUserMessageError(errorMessage) {
+        const messagesToOverwrite = [
+            'Custom auth lambda trigger is not configured for the user pool.'
+        ];
+        if (messagesToOverwrite.includes(errorMessage)) return 'Something went wrong. Check if valid email/password are provided!';
+        else return errorMessage;
     }
 
     showComponent(theme) {
@@ -27,13 +94,27 @@ export default class CustomSignIn extends SignIn {
                     onChangeText={(text) => this.setState({ password: text })}
                 />
                 <View style={styles.buttonContainer}>
-                    <Button type='outline' title='Sign In' buttonStyle={styles.button} titleStyle={styles.buttonTitle} onPress={this.signIn}/>
-                    <Text style={{ color: 'white', marginTop: 25, fontSize: 15 }} onPress={() => this.changeState('signUp')}>Don't have an account? Click here to sign up!</Text>
+                    <Button 
+                        type='outline' 
+                        title='Sign In'
+                        buttonStyle={styles.button} 
+                        titleStyle={styles.buttonTitle} 
+                        disabled={!this.state.username || !this.state.password}
+                        onPress={this.customSignIn}
+                    />
+                    <Text style={{ color: 'white', marginTop: 25, fontSize: 15 }} onPress={() => {
+                        this.setState({
+                            ...this.initialState
+                        }, this.changeState('signUp'));
+                    }}>
+                        Don't have an account? Click here to sign up!
+                    </Text>
                 </View>
-                <Text style={{ color: 'white' }}>{this.state.error === undefined ? '' : this.state.error}</Text>
+                {this.renderAuthSubmitResponse()}
             </KeyboardAvoidingView>
         );
     }
+
 }
 
 const styles = StyleSheet.create({
@@ -55,11 +136,8 @@ const styles = StyleSheet.create({
         justifyContent: 'space-evenly',
     },
     buttonContainer: {
-        flex: 1,
-        maxHeight: 100,
+        maxHeight: 200,
         width: (Dimensions.get('window').width * 0.85),
-        flexDirection: 'column',
-        justifyContent: 'space-evenly',
         alignItems: 'center',
         marginTop: 20
     },
